1ffc57ab7a09596238866ad36cd350a31eed10a1
Ankur Karmakar <ankurkarmakar25126871@gmail.com>
2019-11-29 23:02:01 +0530

    Add files via upload

---
+++ Checked Programs/Banner1.java
@@ -0,0 +1,41 @@
+import java.awt.*;
+import java.applet.*;
+/* 
+<applet code="Banner1.class" width="300" height="300"> 
+</applet> 
+*/  
+public class Banner1 extends Applet implements Runnable {
+   String str = "This is a simple Banner ";
+   Thread t ;
+   boolean b;
+   
+   public void init() {
+      setBackground(Color.gray);
+      setForeground(Color.yellow);
+   }
+   public void start() {
+      t = new Thread(this);
+      b = false;
+      t.start();
+   }
+   public void run () {
+      char ch;
+      for( ; ; ) {
+      try {
+         repaint();
+         Thread.sleep(250);
+         ch = str.charAt(0);
+         str = str.substring(1, str.length());
+         str = str + ch;
+      }
+      catch(InterruptedException e) {}
+      }
+   }
+   public void paint(Graphics g) {
+      g.drawRect(1,1,300,150);
+      g.setColor(Color.yellow);
+      g.fillRect(1,1,300,150);
+      g.setColor(Color.red);
+      g.drawString(str, 1, 150);
+   }
+}
---
+++ Checked Programs/ClientDateTime.java
@@ -0,0 +1,10 @@
+import java.io.*;import java.net.*;
+class ClientDateTime
+{
+	public static void main(String args[]) throws Exception
+	{
+		Socket soc=new Socket(InetAddress.getLocalHost(),5217);
+		BufferedReader in=new BufferedReader(new InputStreamReader(soc.getInputStream()));
+		System.out.println(in.readLine());
+	}
+} 
